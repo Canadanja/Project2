5b95e5a2ff7ef633ff97da7faaf800149b222456
Jannes Klee <jannes.klee@gmail.com>
2014-09-25 12:17:32 +0200

    new

--- main.cpp
+++ main.cpp
@@ -22,26 +22,28 @@ int main()
     double tolerance;
     mat A, R;                                       // Mainmat., Eigenvec.    // 
     vec V;
+    mat D, eigvec;                                  // same for armadillo     //
+    vec eigval;                                     // routine                //  
     
 
-    n=10;                                          // dimension              //
-    tolerance = 1e-10;                              // tolerance              //
+    n=30;                                          // dimension              //
+    tolerance = 1e-15;                              // tolerance              //
     rho_min = 0.;                                   // area of calculation    //
-    rho_max = 5.0;                                   //                        //
-    rho = rho_min;
+    rho_max = 5.0;                                  //                        //
     h_step = (rho_max-rho_min)/n;
     p = 0;
     q = 0;     
 
     // define potential
     V.zeros(n+1);
+    rho = rho_min;
     for (unsigned int i = 0; i <= n; i++)
     {
         V(i) = rho*rho;
         rho += h_step;
     }
 
-   // Jacobi main
+   // Jacobi algorithm -- main part
     A = fillmatrix(n, h_step, V);
     R.eye(n-1,n-1);
     offA = offnorm(A);
@@ -52,12 +54,24 @@ int main()
         offA = offnorm(A);                          // calculate new norm     // 
     }
 
+    // sort the eigenvalues 
+    vec K;
+    K.zeros(n-1);
+    for (unsigned int i = 0; i < n-1; i++)
+    {
+        K(i) = A(i,i);
+    }
+    vec G = sort(K);
+
+    // Armadillo routine for eigenvalues
+    D = fillmatrix(n, h_step, V);                      
+    eig_sym(eigval,eigvec,D);
+
     // output
-//    for (unsigned int i = 0; i < n-1; i++)
-//    {
-//        cout << A(i,i) << endl;
-//    }
-    A.print();
+    for (unsigned int i = 0; i < 9; i++)
+    {
+        cout << A(i,i) <<"    "<< G(i) << "    " << eigval(i) << endl;
+    }
 
     return 0;
 }
@@ -65,12 +79,16 @@ int main()
 mat fillmatrix(unsigned int n, double h_step, vec V)
 {
     mat A(n-1,n-1);
+    double h_step2;
+
     A.zeros(); 
-    A(0,0)=2./(h_step*h_step) + V(1);
+    h_step2 = h_step*h_step;
+
+    A(0,0)=2./(h_step2) + V(1);
     for(unsigned int i = 1; i < n-1; i++){
-        A(i,i) = 2./(h_step*h_step) + V(i+1);
-        A(i,i-1) = -1./(h_step*h_step);
-        A(i-1,i) = -1./(h_step*h_step);
+        A(i,i) = 2./h_step2 + V(i+1);
+        A(i,i-1) = -1./h_step2;
+        A(i-1,i) = -1./h_step2;
     }
     return A;
 }
@@ -83,8 +101,7 @@ double offnorm(mat A)
     n = A.n_rows;                                   // assume symmetry        //    
 
     // loop over (upper) nondiagonal elements
-    //TODO: Get certain about indices. (Compare to searchlargest routine)
-    for (unsigned int i = 1; i < n; i++) {
+    for (unsigned int i = 0; i < n; i++) {
         for (unsigned int j = i+1; j < n; j++) {
                 offA += 2.*A(i,j)*A(i,j);           // 2x for symmetric mat.  // 
         }
@@ -141,7 +158,7 @@ void jacobi(mat &A, mat &R, unsigned int k, unsigned int l)
 
     double akk, all, aik, ail, rik, ril;
     akk = A(k,k);
-    all =A(l,l);
+    all = A(l,l);
     A(k,k) = c*c*akk - 2.0*c*s*A(k,l) + s*s*all ;
     A(l,l) = s*s*akk + 2.0*c*s*A(k,l) + c*c*all;
     A(k,l) = 0.0; // hard-coding non-diagonal elements by hand
@@ -162,4 +179,3 @@ void jacobi(mat &A, mat &R, unsigned int k, unsigned int l)
     }
     return ;
 }
-
